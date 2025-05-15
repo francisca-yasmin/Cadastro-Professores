21b64f69b5154574759c2c9d7b82f11489c424a3
Francisca Yasmin Freitas de Almeida <47915176870@>
2025-05-15 11:34:30 -0300

    inicio do formativo

---
+++ Front_Formativo/src/App.jsx
@@ -0,0 +1,19 @@
+import { BarraNavegacao } from "./Componentes/BarraNavegacao";
+import { Cabecalho } from "./Componentes/Cabecalho";
+import { Conteudo } from "./Componentes/Conteudo";
+import { Footer } from "./Componentes/Footer";
+
+function App() {
+  
+
+  return (
+   <>
+      <BarraNavegacao/>
+      <Cabecalho/>
+      <Conteudo/>
+      <Footer/>
+   </>
+  )
+}
+
+export default App
