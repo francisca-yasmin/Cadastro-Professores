ee04b04025c34ccbae875340d5fe7e1961c7455f
Francisca Yasmin Freitas de Almeida <47915176870@>
2025-05-20 09:43:24 -0300

    front

--- Front_Formativo/src/Componentes/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import estilos from './Footer.module.css'
-
-export function Footer(){
-    return(
-        <p className={estilos.p}> ©Copy; 2025 Bit a Bit. Todos os direitos reservados</p>
-    )
-}
